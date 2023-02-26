4e2f275a509236bc3b9d08790294562908f2ba29
jiyeon950510 <siki4682@naver.com>
2023-02-26 20:03:59 +0900

    개인정보 수정 뷰 생성 및 연결 완료

--- src/main/java/shop/mtcoding/rodongin/controller/EmployeeController.java
+++ src/main/java/shop/mtcoding/rodongin/controller/EmployeeController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployeeController {
 
+    @GetMapping("/infoUpdateForm")
+    public String infoUpdateForm() {
+        return "employee/infoUpdateForm";
+    }
     @GetMapping("/login")
     public String login() {
         return "loginForm";
