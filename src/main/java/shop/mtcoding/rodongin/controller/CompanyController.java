e1cef21e0dac07902575023b3f54025f44720fc6
commulcourse <commulcourse@gmail.com:git config --golbal user.email commulcourse@gmail.comgit config --global user.email commulcourse@gmail.com>
2023-02-27 20:29:02 +0900

    컨트롤러 연결완료

--- src/main/java/shop/mtcoding/rodongin/controller/CompanyController.java
+++ src/main/java/shop/mtcoding/rodongin/controller/CompanyController.java
@@ -22,6 +22,29 @@ public class CompanyController {
     @Autowired
     private HttpSession session;
 
+    // company 로그인요청
+    @PostMapping("/company/login")
+    public String login(CompanyLoginReqDto companyLoginReqDto) {
+        // System.out.println(companyLoginReqDto.getCompanyUsername());
+        // System.out.println(companyLoginReqDto.getCompanyPassword());
+        if (companyLoginReqDto.getCompanyUsername() == null || companyLoginReqDto.getCompanyUsername().isEmpty()) {
+            throw new CustomException("기업아이디를 입력해주세요", HttpStatus.BAD_REQUEST);
+        }
+        if (companyLoginReqDto.getCompanyPassword() == null || companyLoginReqDto.getCompanyPassword().isEmpty()) {
+            throw new CustomException("password를 입력해주세요", HttpStatus.BAD_REQUEST);
+        }
+
+        Company principal = companyRepository.findByCompanyNameAndPassword(companyLoginReqDto);
+
+        if (principal == null) {
+            throw new CustomException("아이디 혹은 비번이 틀렸습니다", HttpStatus.BAD_REQUEST);
+        }
+
+        session.setAttribute("principal", principal);
+
+        return "redirect:/";
+    }
+
     @PostMapping("/company/joinForm")
     public String join() {
         return "company/joinForm";
