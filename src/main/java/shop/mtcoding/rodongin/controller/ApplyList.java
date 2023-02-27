da13a1780faef3630d9ce3274936f0e11b183519
jiyeon950510 <122354247+jiyeon950510@users.noreply.github.com>
2023-02-27 11:10:00 +0900

    Update src/main/java/shop/mtcoding/rodongin/controller/ApplyList.java
    Co-authored-by: kwonkyungryul <68271830+kwonkyungryul@users.noreply.github.com>

--- src/main/java/shop/mtcoding/rodongin/controller/ApplyList.java
+++ src/main/java/shop/mtcoding/rodongin/controller/ApplyList.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ApplyList {
+public class ApplyController {
     
    @GetMapping("/apply/applyList")
     public String applyList() {
