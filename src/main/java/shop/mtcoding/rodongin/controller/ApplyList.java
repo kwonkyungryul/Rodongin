65458177f839a1b1581ea5eba104b695f10cb9a5
jiyeon950510 <122354247+jiyeon950510@users.noreply.github.com>
2023-02-27 11:10:19 +0900

    Update src/main/java/shop/mtcoding/rodongin/controller/ApplyList.java
    Co-authored-by: kwonkyungryul <68271830+kwonkyungryul@users.noreply.github.com>

--- src/main/java/shop/mtcoding/rodongin/controller/ApplyList.java
+++ src/main/java/shop/mtcoding/rodongin/controller/ApplyList.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApplyController {
     
-   @GetMapping("/apply/applyList")
+   @GetMapping("/apply/list")
     public String applyList() {
         return "apply/list";
     }
