fc8b03b8ee64541ae320fec66a31b05879284c8c
Jeonkukil <122415521+Jeonkukil@users.noreply.github.com>
2023-03-01 19:50:38 +0900

    공고리스트데이터바인딩완료 (#22)
    * 공고리스트데이터바인딩완료

---
+++ src/main/java/shop/mtcoding/rodongin/dto/announcement/AnnouncementReq.java
@@ -0,0 +1,37 @@
+package shop.mtcoding.rodongin.dto.announcement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class AnnouncementReq {
+    @Getter
+    @Setter
+    public static class AnnouncementSaveReqDto {
+        private Integer stackId;
+        private String announcementTitle;
+        private String announcementContent;
+        private String announcementCarrer;
+        private String announcementHireType;
+        private int announcementRecNum;
+        private String announcementSalary;
+        private String announcementArea;
+    }
+
+    @Getter
+    @Setter
+    public static class AnnouncementUpdateReqDto {
+        private Integer stackId;
+        private String announcementTitle;
+        private String announcementContent;
+        private String announcementCarrer;
+        private String announcementHireType;
+        private Integer announcementRecNum;
+        private String announcementSalary;
+        private String announcementArea;
+    }
+
+    }
+
+
+
+
---
+++ src/main/java/shop/mtcoding/rodongin/model/announcement/AnnouncementRepository.java
@@ -0,0 +1,43 @@
+package shop.mtcoding.rodongin.model.announcement;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import shop.mtcoding.rodongin.dto.announcement.AnnouncementReq.AnnouncementUpdateReqDto;
+import shop.mtcoding.rodongin.dto.announcement.AnnouncementResp.AnnouncementDetailReqDto;
+
+
+
+// 명사가 뒤 동사가 앞으로
+@Mapper
+public interface AnnouncementRepository {
+
+
+    public AnnouncementDetailReqDto findAnnouncementAndCompanyId(int id);
+
+    public List<AnnouncementDetailReqDto> findAnnouncementlist();
+    // public List<AnnouncementDetailReqDto> findByDetailUser(int id);
+
+    public List<Announcement> findTopList();
+
+    public List<Announcement> findAll();
+
+    public Announcement findById(int id);
+
+     
+    public int insert(@Param("companyId") int companyId, 
+                    @Param("stackId") int stackId, 
+                    @Param("announcementTitle") String announcementTitle,
+                    @Param("announcementContent") String announcementContent,
+                    @Param("announcementCarrer") String announcementCarrer,
+                    @Param("announcementHireType") String announcementHireType,
+                    @Param("announcementRecNum") int announcementRecNum,
+                    @Param("announcementSalary") String announcementSalary,
+                    @Param("announcementArea") String announcementArea);
+                    
+                    public int updateById(AnnouncementUpdateReqDto announcementUpdateReqDto, int principalId);
+
+    public int deleteById(int id);
+}
