ed29c43726ad78e3acb813a6c0ac1631e08528dd
kkr <kkr0787@naver.com>
2023-03-03 16:44:23 +0900

    인증 체크 추가

--- src/main/java/shop/mtcoding/rodongin/controller/ApplyController.java
+++ src/main/java/shop/mtcoding/rodongin/controller/ApplyController.java
@@ -1,7 +1,8 @@
 package shop.mtcoding.rodongin.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,12 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import shop.mtcoding.rodongin.handler.ex.CustomException;
 import shop.mtcoding.rodongin.dto.apply.ApplyRes.ApplyListResDto;
-import shop.mtcoding.rodongin.dto.apply.ApplyRes.ApplyListResumeResDto;
-import shop.mtcoding.rodongin.model.apply.Apply;
 import shop.mtcoding.rodongin.model.apply.ApplyRepository;
-import shop.mtcoding.rodongin.model.resume.Resume;
-import shop.mtcoding.rodongin.model.resume.ResumeGraduate;
+import shop.mtcoding.rodongin.model.company.Company;
 import shop.mtcoding.rodongin.model.resume.ResumeGraduateRepository;
 import shop.mtcoding.rodongin.model.resume.ResumeRepository;
 
@@ -30,11 +29,15 @@ public class ApplyController {
     @Autowired
     ResumeGraduateRepository resumeGraduateRepository;
 
-    // @Autowired
-    // ResumeCareerRepository resumeCareerRepository;
+    @Autowired
+    HttpSession session;
 
     @GetMapping("/apply/{companyId}/list")
     public String applyList(@PathVariable int companyId, Model model) {
+        Company principal = (Company) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomException("인증되지 않았습니다.");
+        }
         List<ApplyListResDto> applys = applyRepository.findByCompanyId(companyId);
 
         model.addAttribute("applys", applys);
