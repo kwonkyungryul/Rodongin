e19ee64ff43861fd658addb0b37523085be2f611
kkr <kkr0787@gmail.com>
2023-03-04 16:33:52 +0900

    dev test

--- src/main/java/shop/mtcoding/rodongin/controller/ApplyController.java
+++ src/main/java/shop/mtcoding/rodongin/controller/ApplyController.java
@@ -11,8 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PathVariable;asdasdas
 import shop.mtcoding.rodongin.handler.ex.CustomException;
 import shop.mtcoding.rodongin.dto.apply.ApplyRes.ApplyListResDto;
 import shop.mtcoding.rodongin.model.apply.ApplyRepository;
