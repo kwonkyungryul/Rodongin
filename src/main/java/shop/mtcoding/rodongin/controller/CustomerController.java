cb8914f7267600c96ada262c9201d73c116c9204
commulcourse <commulcourse@gmail.com:git config --golbal user.email commulcourse@gmail.comgit config --global user.email commulcourse@gmail.com>
2023-03-05 15:09:23 +0900

    save 컨트롤러 생성

--- src/main/java/shop/mtcoding/rodongin/controller/CustomerController.java
+++ src/main/java/shop/mtcoding/rodongin/controller/CustomerController.java
@@ -1,14 +1,41 @@
 package shop.mtcoding.rodongin.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.rodongin.dto.customer.CustomerReq.CustomerSaveReqDto;
+import shop.mtcoding.rodongin.handler.ex.CustomException;
+import shop.mtcoding.rodongin.model.customer.Customer;
 
 @Controller
 public class CustomerController {
 
-    @GetMapping("/customer")
+    @PostMapping("/customer")
+    public String save(CustomerSaveReqDto customerSaveReqDto) {
+        Customer principal = (Customer) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
+        }
+        if (CustomerSaveReqDto.getCu.length() > 100) {
+            throw new CustomException("title의 길이가 100자 이하여야 합니다");
+        }
+        // if (CustomerSaveReqDto.getCustomerContent() == null ||
+        // BoardSaveReqDto.getCustomerContent().isEmpty()) {
+        // throw new CustomException("content를 작성해주세요");
+    }
+
+    CustomerService.글쓰기(BoardSaveReqDto,principal.getId());
+
+    return"redirect:/";
+
+    }
+
+    @GetMapping("/customer/list")
     public String list(Model model) {
         return "customer/list";
     }
+
 }
